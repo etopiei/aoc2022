7f65926ebb2313c7234d44229d057c42f97c5bf8
Lachlan Jacob <lachlan.jacob@wspdigital.com>
2022-12-02 17:03:15 +1100

    Day 2 done!
    I made one small mistake with data structures at one point (doing rev and hd in the wrong order)
    but overall found this day much easier now that I am getting used to the language on the whole and
    the tooling around it.
    
    I am nervous that the problems may get less conducive to this sort of programming though. At the moment
    parsing and processing is easy with functional control strucutres, but it may get harder if we need
    to index into arrays or anything. Guess we will see?
    
    For now though really enjoying this and found the main problem trying to work out Rock paper scissors!
    I wasted a long time debugging an issue that turned out to be just incorrect transcription of the game results!
    So that was kind of annoying, but I guess I should've made a data structure with better names :shrug:
    Still need to learn some more about that stuff with Ocaml.

---
+++ day2/part1.ml
@@ -0,0 +1,36 @@
+
+let file = "input";;
+let file_contents = In_channel.with_open_text file In_channel.input_all;;
+let games = String.split_on_char '\n' file_contents;;
+let game_tuples = List.map (String.split_on_char ' ') games;;
+
+
+let opponent_choices = List.map List.hd game_tuples;;
+let my_choices = List.map (fun a -> List.nth a 1) game_tuples;;
+
+
+let score_game opponent_choice player_choice = match (opponent_choice, player_choice) with
+  | ('B', 'X') -> 1
+  | ('C', 'Y') -> 2
+  | ('A', 'Z') -> 3
+  | ('A', 'X') -> 4 
+  | ('B', 'Y') -> 5
+  | ('C', 'Z') -> 6
+  | ('C', 'X') -> 7
+  | ('A', 'Y') -> 8 
+  | ('B', 'Z') -> 9
+  | _ -> 0
+;;
+
+let games = Seq.zip (List.to_seq opponent_choices) (List.to_seq my_choices);;
+
+let get_char char_string = List.hd (List.of_seq (String.to_seq char_string));;
+
+let scores = Seq.map (fun (a, b) -> score_game (get_char a) (get_char b)) games;;
+
+let total_score = Seq.fold_left (+) 0 scores;;
+
+print_int total_score
+
+
+
